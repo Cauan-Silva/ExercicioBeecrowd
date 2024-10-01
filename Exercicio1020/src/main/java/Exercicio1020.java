87fcf2eb7121235283ac7f7e7384db5170f78114
Cauan-Silva <anacauansantos@gmail.com>
2024-10-01 18:46:47 -0300

    Exercicio1020

---
+++ Exercicio1020/src/main/java/Exercicio1020.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exercicio1020 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int idadeEmDias = scanner.nextInt();
+        
+        int anos = idadeEmDias / 365;
+        int meses = (idadeEmDias % 365) / 30;
+        int dias = (idadeEmDias % 365) % 30;
+        
+        System.out.printf("%d ano(s)%n", anos);
+        System.out.printf("%d mes(es)%n", meses);
+        System.out.printf("%d dia(s)%n", dias);
+    }
+}
